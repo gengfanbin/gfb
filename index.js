dd83bdf9fe7adacc67127d23889f4e3794e9fd19
gengfanbin <gengfanbin@163.com>
2022-08-02 13:35:30 +0800

    优化测试案例

--- index.js
+++ index.js
@@ -14,9 +14,12 @@ class testClass extends GFB {
     let test = this.State.test + (parseFloat( number)||1)
     this.Update({test})
   }
+  clear(){
+    this.Update({test:0})
+  }
   rendertest(){
     let elm = ''
-    for(let i = 0; i<10; i++){
+    for(let i = 0; i<this.State.test; i++){
       elm += `<div>${i}</div>`
     }
     return elm
@@ -52,6 +55,9 @@ class testClass extends GFB {
         </div>
         <div>
           <button ref="testdiv2" on:click="{%'clickdiv1(2)'%}" >button2</button>
+        </div>
+        <div>
+          <button ref="testdiv2" on:click="{%'clear()'%}" >clear</button>
         </div>
       </div>
     `
