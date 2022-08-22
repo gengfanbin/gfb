7a540a43d82efc8c765d00586b8d889a5860f70b
gengfanbin <gengfanbin@163.com>
2022-08-22 10:18:08 +0800

    add Introduction

--- Page/Introduction/index.js
+++ Page/Introduction/index.js
@@ -2,11 +2,38 @@ class Introduction extends GFB.Component {
   constructor(){
     super()
   }
+
+  download(link){
+    window.open(link)
+  }
   
   Render(){
     return`
       <div class="Introduction">
-        <div></div>
+        <div class="top">
+          <div class="logo_box">
+            <img src="./logo_w.png" class="logo" />
+            <span>G</span><span>F</span><span>B</span>
+          </div>
+          <div class="tips_box">
+            <div class="tips">
+              <span>慷慨（Generousa）</span>
+              <span>它可以以库的形式将自己慷慨的奉献给其他框架</span>
+            </div>
+            <div class="tips">
+              <span>流畅（Fluent）</span>
+              <span>它使用原生DOM，在无论组件下挂载多少子组件，也只会进行一次DOM渲染</span>
+            </div>
+            <div class="tips">
+              <span>简明（Brief）</span>
+              <span>它使用原生JavaScript语法，开发者只需要了解一下它的基础API即可进行开发</span>
+            </div>
+          </div>
+        </div>
+        <div class="download_box">
+          <span class="download_button" on:click="download('https://www.gfb-js.com/gfb.js')">开发版下载</span>
+          <span class="download_button" on:click="download('https://www.gfb-js.com/gfb-min.js')">生产版下载</span>
+        </div>
       </div>
     `
   }
