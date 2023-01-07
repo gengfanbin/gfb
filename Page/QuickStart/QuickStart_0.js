36713f04dc4676dfd112dfa3209c038098ca4b25
gengfanbin <gengfanbin@163.com>
2023-01-07 10:07:22 +0800

    edit inline import link url

--- Page/QuickStart/QuickStart_0.js
+++ Page/QuickStart/QuickStart_0.js
@@ -3,13 +3,15 @@ let QuickStart_0 = {
       type: "开始",
       content: `<div>
         完整示例: <a href="https://gitee.com/gengfanbin/gfb-example" target="_blank">https://gitee.com/gengfanbin/gfb-example</a><br/><br/>
+        <div>如果你使用过React，你会发现GFB的语法与React的class组件很相似。</div><br/>
+        <div>但需要注意，GFB并不是React的翻版，它没有虚拟DOM，也不存在类似ReactNode等概念，GFB处理的是描述DOM结构的javaScript字符串。</div><br/>
       </div>`,
       sub: [
         {
           key: 01, text: "在浏览器环境",
           content: `<div>
 <pre>
-&lt;script src="http://www.gfb-js.com/gfb.js">&lt;/script>
+&lt;script src="https://www.gfb-js.com/GFB/gfb.js">&lt;/script>
 </pre>
         </div>`},
         {
